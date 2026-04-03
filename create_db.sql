b61dd02765f033e49be64a93648e7213d340c020
Isabelle Cappiello <icappiello8880@floridapoly.edu>
2026-04-02 21:09:16 -0400

    Uploaded Part D files
    (I did not complete dataload.py, I could not complete it.... well, not in time, but before assignment closed)

---
+++ create_db.sql
@@ -0,0 +1,90 @@
+CREATE TABLE LOC(
+    Loc_ID VARCHAR2(10) PRIMARY KEY,
+    Loc_Start_Lat NUMBER NOT NULL,
+    Loc_Start_Lng NUMBER NOT NULL,
+    Loc_End_Lat NUMBER,
+    Loc_End_Lng NUMBER
+);
+
+CREATE TABLE COORDINATE(
+    Loc_Start_Lat FLOAT NOT NULL,
+    Loc_Start_Lng FLOAT NOT NULL,
+    Loc_Street VARCHAR2(50),
+    Loc_City VARCHAR2(50),
+    Loc_County VARCHAR2(50) NOT NULL,
+    Loc_State VARCHAR2(50) NOT NULL,
+    Loc_Country VARCHAR2(10) NOT NULL,
+    Loc_Timezone VARCHAR2(20),
+    Loc_Airport_Code VARCHAR2(10),
+    CONSTRAINT PK_Coordinate PRIMARY KEY(Loc_Start_Lat, Loc_Start_Lng)
+);
+
+CREATE TABLE TIM(
+    Time_ID VARCHAR2(10) PRIMARY KEY,
+    Time_Start TIMESTAMP NOT NULL,
+    Time_End TIMESTAMP NOT NULL
+);
+
+CREATE TABLE SEVERITY(
+    Time_Start TIMESTAMP NOT NULL,
+    Time_End TIMESTAMP NOT NULL,
+    Acc_Severity NUMBER NOT NULL,
+    CONSTRAINT PK_Severity PRIMARY KEY(Time_Start, Time_End)
+);
+
+CREATE TABLE ACCIDENT(
+    Acc_ID VARCHAR2(10) PRIMARY KEY,
+    Time_ID VARCHAR2(10) NOT NULL,
+    Loc_ID VARCHAR2(10) NOT NULL,
+    Acc_Source	VARCHAR2(20) NOT NULL,
+    Acc_Description  VARCHAR2(500),
+    Acc_Distance FLOAT NOT NULL,
+    CONSTRAINT UK_Accident_Time_Location UNIQUE(Time_ID, Loc_ID)
+);
+
+CREATE TABLE WEATHER(
+    Time_ID VARCHAR2(10) NOT NULL,
+    Loc_ID VARCHAR2(10) NOT NULL,
+    Wea_Timestamp TIMESTAMP,
+    Wea_Temperature FLOAT,
+    Wea_Condition VARCHAR2(20),
+    Wea_Wind_Chill FLOAT,
+    Wea_Humidity NUMBER,
+    Wea_Visibility FLOAT,
+    Wea_Wind_Direction VARCHAR2(10),
+    Wea_Wind_Speed FLOAT,
+    Wea_Pressure FLOAT,
+    Wea_Speed FLOAT,
+    Wea_Precipitation FLOAT,
+    Wea_Sunrise_Sunset VARCHAR2(10),
+    Wea_Civil_Twilight VARCHAR2(10),
+    Wea_Nautical_Twilight VARCHAR2(10),
+    Wea_Astronomical_Twilight VARCHAR2(10),
+    Constraint PK_Weather PRIMARY KEY(Time_Id,Loc_ID)
+);
+
+--Foriegn Key Alterations
+ALTER TABLE ACCIDENT
+ADD CONSTRAINT FK_ACCIDENT_LOCATION
+FOREIGN KEY (Loc_ID)
+REFERENCES LOC(Loc_ID);
+
+ALTER TABLE WEATHER
+ADD CONSTRAINT FK_WEATHER_TIME
+FOREIGN KEY (Time_ID)
+REFERENCES TIM(Time_ID);
+
+ALTER TABLE WEATHER
+ADD CONSTRAINT FK_WEATHER_LOCATION
+FOREIGN KEY (Loc_ID)
+REFERENCES LOC(Loc_ID);
+
+ALTER TABLE TIM
+ADD CONSTRAINT FK_TIME_SEVERITY
+FOREIGN KEY (Time_Start, Time_End)
+REFERENCES SEVERITY(Time_Start, Time_End);
+
+ALTER TABLE LOC
+ADD CONSTRAINT FK_Loc_Coordinate
+FOREIGN KEY (Loc_Start_Lat,Loc_Start_Lng)
+REFERENCES COORDINATE(Loc_Start_Lat,Loc_Start_Lng);
---
+++ preprocess.py
@@ -0,0 +1,140 @@
+import pandas as pd
+import csv
+from datetime import datetime, timedelta
+
+# input file
+input_file = r"Small_US_Accidents_March23.csv"
+
+# read csv
+df = pd.read_csv(input_file)
+
+# 1. Severity information csv
+severity_df = df[
+    [
+        "Start_Time",
+        "End_Time",
+        "Severity",
+    ]
+].drop_duplicates(subset=["Start_Time","End_Time"]).copy()
+
+# 2. time information csv
+time_df = df[
+    [
+        "Start_Time",
+        "End_Time",
+    ]
+].drop_duplicates(subset=["Start_Time","End_Time"]).copy()
+time_df.insert(0,"Time_ID",range(1,len(time_df)+1))
+
+# 3. coordinate information csv
+coordinate_df = df[
+    [
+        "Start_Lat",
+        "Start_Lng",
+        "Street",
+        "City",
+        "County",
+        "State",
+        "Zipcode",
+        "Country",
+        "Timezone",
+        "Airport_Code",
+    ]
+].drop_duplicates(subset=["Start_Lat","Start_Lng"]).copy()
+coordinate_df = coordinate_df.fillna("N/A")
+
+# 4. location information csv
+location_df = df[
+    [
+        "Start_Lat",
+        "Start_Lng",
+        "End_Lat",
+        "End_Lng",
+    ]
+].drop_duplicates(subset=["Start_Lat","Start_Lng"]).copy()
+location_df.insert(0,"Location_ID",range(1,len(location_df)+1))
+location_df = location_df.fillna(0)
+
+# 5. accident information csv
+temp_accident_df = df[
+    [
+        "ID",
+        "Source",
+        "Distance(mi)",
+        "Description",
+        "Start_Lat",
+        "Start_Lng",
+        "Start_Time",
+        "End_Time",
+    ]
+].drop_duplicates(subset=["ID"]).copy()
+temp_accident_df = pd.merge(temp_accident_df,location_df, "left", on=["Start_Lat","Start_Lng"])
+temp_accident_df = pd.merge(temp_accident_df,time_df, "left", on=["Start_Time","End_Time"])
+
+accident_df = temp_accident_df[
+    [
+        "ID",
+        "Location_ID",
+        "Time_ID",
+        "Source",
+        "Distance(mi)",
+        "Description",
+    ]
+].drop_duplicates("ID").copy()
+accident_df = accident_df.fillna("N/A")
+
+# 6. weather features information csv
+temp_weather_df = df[
+    [
+        "Start_Lat",
+        "Start_Lng",
+        "Start_Time",
+        "End_Time",
+        "Weather_Timestamp",
+        "Temperature(F)",
+        "Wind_Chill(F)",
+        "Humidity(%)",
+        "Pressure(in)",
+        "Visibility(mi)",
+        "Wind_Direction",
+        "Wind_Speed(mph)",
+        "Precipitation(in)",
+        "Sunrise_Sunset",
+        "Civil_Twilight",
+        "Nautical_Twilight",
+        "Astronomical_Twilight",
+    ]
+]
+temp_weather_df = pd.merge(temp_weather_df,location_df, "left", on=["Start_Lat","Start_Lng"])
+temp_weather_df = pd.merge(temp_weather_df,time_df, "left", on=["Start_Time","End_Time"])
+
+weather_df = temp_weather_df[
+    [
+        "Location_ID",
+        "Time_ID",
+        "Weather_Timestamp",
+        "Temperature(F)",
+        "Wind_Chill(F)",
+        "Humidity(%)",
+        "Pressure(in)",
+        "Visibility(mi)",
+        "Wind_Direction",
+        "Wind_Speed(mph)",
+        "Precipitation(in)",
+        "Sunrise_Sunset",
+        "Civil_Twilight",
+        "Nautical_Twilight",
+        "Astronomical_Twilight",
+    ]
+].drop_duplicates(subset=["Location_ID","Time_ID"]).copy()
+weather_df = weather_df.fillna(0)
+
+# save files
+accident_df.to_csv(r"data\accident_information.csv", index=False)
+time_df.to_csv(r"data\time_information.csv", index=False)
+severity_df.to_csv(r"data\severity_information.csv", index=False)
+location_df.to_csv(r"data\location_information.csv", index=False)
+coordinate_df.to_csv(r"data\coordinate_information.csv", index=False)
+weather_df.to_csv(r"data\weather_information.csv", index=False)
+
+print("CSV files successfully generated.")
