323e2cce8c10c4e913460c388ecff9c422b94583
Isabelle Cappiello <icappiello8880@floridapoly.edu>
2026-04-06 22:54:14 -0400

    Update preprocess.py
    Fixed inconsistencies

--- preprocess.py
+++ preprocess.py
@@ -35,13 +35,11 @@ coordinate_df = df[
         "City",
         "County",
         "State",
-        "Zipcode",
         "Country",
         "Timezone",
         "Airport_Code",
     ]
 ].drop_duplicates(subset=["Start_Lat","Start_Lng"]).copy()
-coordinate_df = coordinate_df.fillna("N/A")
 
 # 4. location information csv
 location_df = df[
@@ -53,15 +51,14 @@ location_df = df[
     ]
 ].drop_duplicates(subset=["Start_Lat","Start_Lng"]).copy()
 location_df.insert(0,"Location_ID",range(1,len(location_df)+1))
-location_df = location_df.fillna(0)
 
 # 5. accident information csv
 temp_accident_df = df[
     [
         "ID",
         "Source",
-        "Distance(mi)",
         "Description",
+        "Distance(mi)",
         "Start_Lat",
         "Start_Lng",
         "Start_Time",
@@ -74,14 +71,13 @@ temp_accident_df = pd.merge(temp_accident_df,time_df, "left", on=["Start_Time","
 accident_df = temp_accident_df[
     [
         "ID",
-        "Location_ID",
         "Time_ID",
+        "Location_ID",
         "Source",
-        "Distance(mi)",
         "Description",
+        "Distance(mi)",
     ]
 ].drop_duplicates("ID").copy()
-accident_df = accident_df.fillna("N/A")
 
 # 6. weather features information csv
 temp_weather_df = df[
@@ -92,17 +88,14 @@ temp_weather_df = df[
         "End_Time",
         "Weather_Timestamp",
         "Temperature(F)",
+        "Weather_Condition",
         "Wind_Chill(F)",
         "Humidity(%)",
-        "Pressure(in)",
         "Visibility(mi)",
         "Wind_Direction",
         "Wind_Speed(mph)",
+        "Pressure(in)",
         "Precipitation(in)",
-        "Sunrise_Sunset",
-        "Civil_Twilight",
-        "Nautical_Twilight",
-        "Astronomical_Twilight",
     ]
 ]
 temp_weather_df = pd.merge(temp_weather_df,location_df, "left", on=["Start_Lat","Start_Lng"])
@@ -110,24 +103,20 @@ temp_weather_df = pd.merge(temp_weather_df,time_df, "left", on=["Start_Time","En
 
 weather_df = temp_weather_df[
     [
-        "Location_ID",
         "Time_ID",
+        "Location_ID",
         "Weather_Timestamp",
         "Temperature(F)",
+        "Weather_Condition",
         "Wind_Chill(F)",
         "Humidity(%)",
-        "Pressure(in)",
         "Visibility(mi)",
         "Wind_Direction",
         "Wind_Speed(mph)",
+        "Pressure(in)",
         "Precipitation(in)",
-        "Sunrise_Sunset",
-        "Civil_Twilight",
-        "Nautical_Twilight",
-        "Astronomical_Twilight",
     ]
 ].drop_duplicates(subset=["Location_ID","Time_ID"]).copy()
-weather_df = weather_df.fillna(0)
 
 # save files
 accident_df.to_csv(r"data\accident_information.csv", index=False)
